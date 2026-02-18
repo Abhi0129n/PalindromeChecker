1d6be6a521708d8c3ea21e28ba5dc1fef3e39c4c
abhi <sheka5312@gmail.com>
2026-02-18 09:44:21 +0530

    abhi Added UsecaseName

--- PalindromeCheckerApp/src/palindromeChekerApp.java
+++ PalindromeCheckerApp/src/palindromeChekerApp.java
@@ -1,5 +1,8 @@
-public class palindromeChekerApp {
+
+public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
